ce46421a95a3a6ee959a74d49b77bdedd2896a9e
bikash.k <bikash.k@LP-5CD6495TH7.HCLC.CORP.HCL.IN>
2018-11-29 16:59:20 +0530

    # WARNING: head commit changed in the meantime
    test

--- src/main/java/com/hcl/dprism/config/WebSecurityConfig.java
+++ src/main/java/com/hcl/dprism/config/WebSecurityConfig.java
@@ -26,7 +26,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").access("hasAnyRole('USER', 'ADMIN')")				
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/").access("hasAnyRole('USER', 'ADMIN')")					
+				.antMatchers("/css/**", "/images/**","/**/*.js").permitAll()				
 				.anyRequest().authenticated().and().formLogin()
 				.loginPage("/login").permitAll().and().logout().permitAll();
 	}
