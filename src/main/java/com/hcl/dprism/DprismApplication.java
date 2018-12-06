f55c59f2a53e14ac231e252791641aa472c7786a
bikash.k <bikash.k@LP-5CD6495TH7.HCLC.CORP.HCL.IN>
2018-12-06 13:22:12 +0530

    role config changes

--- src/main/java/com/hcl/dprism/DprismApplication.java
+++ src/main/java/com/hcl/dprism/DprismApplication.java
@@ -2,9 +2,11 @@ package com.hcl.dprism;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 /**
  * Dprism Application
  * @author bikash.k
@@ -12,11 +14,20 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
  */
 //@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @SpringBootApplication
-@ComponentScan({"com.hcl.dprism.*"})
+@ComponentScan({"com.hcl.dprism.*,org.thymeleaf.extras.springsecurity4"})
 //@Component("com.hcl.dprism.*")
 public class DprismApplication {
 	
    	public static void main(String[] args) {
 		SpringApplication.run(DprismApplication.class, args);
 	}
+   	
+   	
+   	@Bean
+   	public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver, SpringSecurityDialect sec) {
+   	    final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+   	    templateEngine.setTemplateResolver(templateResolver);
+   	    templateEngine.addDialect(sec); // Enable use of "sec"
+   	    return templateEngine;
+   	}	
 }
